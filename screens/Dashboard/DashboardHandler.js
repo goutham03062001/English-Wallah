ffe1a4c72a99bccbacf7bc693e2a5170d3704d68
Goutham <gouthamkumarpolapally@gmail.com>
2023-12-25 18:22:21 +0530

    dashboard handler file changed

--- screens/Dashboard/DashboardHandler.js
+++ screens/Dashboard/DashboardHandler.js
@@ -13,14 +13,11 @@ import PronounStartingScreen from './Academic/Pronoun/PronounStartingScreen';
 import AdjectiveStartingScreen from './Academic/Adjective/AdjectiveStartingScreen';
 import AdverbStartingScreen from './Academic/Adverb/AdverbStartingScreen';
 import ConjunctionStartingScreen from './Academic/Conjunction/ConjunctionStartingScreen';
-<<<<<<< HEAD
-=======
 import Verb from './Competative/ModelPapers/Verb';
 import VerbStartingScreen from './Academic/Verb/VerbStartingScreen';
 import ActiveAndPassiveStartingScreen from './Academic/ActiveAndPassive/ActiveAndPassiveStartingScreen';
 import PrepositionStartingScreen from './Academic/Preposition/PrepositionStartingScreen';
 import ArticlesStartingScreen from "./Academic/Articles/ArticlesStartingScreen"
->>>>>>> 0a28a32 (version 1.0.7)
 const DashboardHandler = () => {
     const authCtx = useContext(AuthContext);
   return (
@@ -34,14 +31,11 @@ const DashboardHandler = () => {
     <Stack.Screen name = "Adjective" component={AdjectiveStartingScreen} options={{headerShown:false}}/>
     <Stack.Screen name = "Adverb" component={AdverbStartingScreen} options={{headerShown:false}}/>
     <Stack.Screen name = "Conjunction" component={ConjunctionStartingScreen} options={{headerShown:false}}/>
-<<<<<<< HEAD
-=======
     <Stack.Screen name = "Verb" component={VerbStartingScreen} options={{headerShown:false}}/>
     <Stack.Screen name = "Active And Passive Voice" component={ActiveAndPassiveStartingScreen} options={{headerShown:false}}/>
     <Stack.Screen name = "Preposition" component={PrepositionStartingScreen} options={{headerShown:false}}/>
     <Stack.Screen name = "Articles" component={ArticlesStartingScreen} options={{headerShown:false}}/>
     
->>>>>>> 0a28a32 (version 1.0.7)
     </Stack.Navigator>
   )
 }
